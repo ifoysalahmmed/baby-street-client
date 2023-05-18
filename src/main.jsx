49dc32aec81a43c67a2a02cfae7a1242e0c679ca
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-05-18 22:54:43 +0600

    added Context Component

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes";
+import AuthProvider from "./Context/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div className="bg-green-100">
     <div className="max-w-7xl mx-auto ">
       <React.StrictMode>
-        <RouterProvider router={router}></RouterProvider>
+        <AuthProvider>
+          <RouterProvider router={router}></RouterProvider>
+        </AuthProvider>
       </React.StrictMode>
     </div>
   </div>
