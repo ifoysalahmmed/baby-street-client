faae3b241d840cbeb2ffa032fae86b45b0ab4ecb
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-05-18 22:20:37 +0600

    added SignUp Component

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,34 +1,60 @@
 import { Link } from "react-router-dom";
 import signInImg from "../../assets/login/signIn.png";
 
-const Login = () => {
-  const handleLogin = (event) => {
+const SignUp = () => {
+  const handleSignUp = (event) => {
     event.preventDefault();
 
     const form = event.target;
 
+    const name = form.name.value;
+    const photo = form.photo.value;
     const email = form.email.value;
     const password = form.password.value;
 
-    console.log(email, password);
+    console.log(name, photo, email, password);
   };
 
   return (
     <div className="hero min-h-screen bg-green-200 mt-8">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="w-1/2 ml-12 space-y-8 text-center">
-          <h1 className="text-5xl font-bold w-full">Login now!</h1>
+          <h1 className="text-5xl font-bold w-full">Sign Up now!</h1>
           <img src={signInImg} alt="" />
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form onSubmit={handleLogin} className="card-body">
+          <form onSubmit={handleSignUp} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Your Name"
+                name="name"
+                required
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo URL</span>
+              </label>
+              <input
+                type="url"
+                placeholder="Photo URL"
+                name="photo"
+                required
+                className="input input-bordered"
+              />
+            </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
                 type="email"
-                placeholder="email"
+                placeholder="Your Email"
                 name="email"
                 required
                 className="input input-bordered"
@@ -40,24 +66,19 @@ const Login = () => {
               </label>
               <input
                 type="password"
-                placeholder="password"
+                placeholder="Your Password"
                 name="password"
                 required
                 className="input input-bordered"
               />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+              <button className="btn btn-primary">Sign Up</button>
             </div>
             <p className="my-4 text-center">
-              New to Baby Street?{" "}
-              <Link to="/signUp" className="text-green-600 font-bold">
-                Sign up
+              Already have an account?{" "}
+              <Link to="/login" className="text-green-600 font-bold">
+                Login
               </Link>
             </p>
           </form>
@@ -67,4 +88,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
