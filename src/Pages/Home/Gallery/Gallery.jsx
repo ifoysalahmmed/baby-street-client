70e4b08c9ec86494fc88a0ccf301bf68bcb8128d
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-05-19 00:33:23 +0600

    added Gallery Component

---
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -0,0 +1,79 @@
+const Gallery = () => {
+  return (
+    <div className="container mx-auto px-5 md:px-0 py-2 pt-8 md:pt-12">
+      <div className="-m-1 md:flex flex-wrap md:-m-2">
+        <div className="md:flex md:w-1/2 flex-wrap">
+          <div className="md:w-1/2 p-1 md:p-2">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://images.unsplash.com/photo-1591438252948-fa5dd3701c2a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+            />
+          </div>
+          <div className="md:w-1/2 p-1 md:p-2">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://images.unsplash.com/photo-1617808654153-9b20f8728b02?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1023&q=80"
+            />
+          </div>
+          <div className="w-full p-1 md:p-2">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://images.unsplash.com/photo-1658490261482-185ef4add566?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+            />
+          </div>
+        </div>
+        <div className="md:flex md:w-1/2 flex-wrap">
+          <div className="w-full p-1 md:p-2">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://images.unsplash.com/photo-1529651490292-99f6f9ade9e8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+            />
+          </div>
+          <div className="md:w-1/2 p-1 md:p-2">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://images.unsplash.com/photo-1566207962290-9e3cc51f4199?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=763&q=80"
+            />
+          </div>
+          <div className="md:w-1/2 p-1 md:p-2">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://images.unsplash.com/photo-1654155917394-cc476faca27d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+            />
+          </div>
+        </div>
+        <div className="md:flex md:w-full flex-wrap">
+          <div className="md:w-1/3 p-1 md:p-2">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://images.unsplash.com/photo-1447931958677-954446df5f70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+            />
+          </div>
+          <div className="md:w-1/3 p-1 md:p-2">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://images.unsplash.com/photo-1666981284405-fcaa9f5c67b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+            />
+          </div>
+          <div className="md:w-1/3 p-1 md:p-2">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://images.unsplash.com/photo-1674717397031-3841482f25d1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1032&q=80"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
