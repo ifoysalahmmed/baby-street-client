7f05c872b0acdd19142174b49f78326f7586d20b
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-05-19 16:15:16 +0600

    added Search in AllToys & Rating in Toy Components

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -3,34 +3,59 @@ import AllToysRow from "./AllToysRow";
 
 const AllToys = () => {
   const [allToys, setAllToys] = useState([]);
+  const [searchName, setSearchName] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:5000/allToys")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
         setAllToys(data);
       });
   }, []);
 
+  const handleSearch = () => {
+    fetch(`http://localhost:5000/getToysByName/${searchName}`)
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        setAllToys(data);
+      });
+  };
+
   return (
-    <div className="overflow-x-auto w-full rounded-md text-center mt-12">
-      <table className="table-normal w-full ">
-        <thead className="bg-green-400">
-          <th></th>
-          <th>Seller Name</th>
-          <th>Toy Name</th>
-          <th>Category</th>
-          <th>Price</th>
-          <th>Available Quantity</th>
-          <th></th>
-        </thead>
-        <tbody className="bg-green-200 text-black">
-          {allToys?.map((toy) => (
-            <AllToysRow key={toy._id} toy={toy}></AllToysRow>
-          ))}
-        </tbody>
-      </table>
+    <div>
+      <div className="sm:text-center mt-6 md:flex items-center justify-center gap-4">
+        <input
+          onChange={(e) => setSearchName(e.target.value)}
+          type="text"
+          placeholder="Toy Name"
+          className="input w-full max-w-xs"
+        />
+        <button onClick={handleSearch} className="btn btn-success">
+          Search
+        </button>
+      </div>
+      <div className="overflow-x-auto w-full rounded-md text-center mt-6">
+        <table className="table-normal w-full ">
+          <thead className="bg-green-400">
+            <tr>
+              <th></th>
+              <th>Seller Name</th>
+              <th>Toy Name</th>
+              <th>Category</th>
+              <th>Price</th>
+              <th>Available Quantity</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody className="bg-green-200 text-black">
+            {allToys?.map((toy) => (
+              <AllToysRow key={toy._id} toy={toy}></AllToysRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
--- src/Pages/Home/Toy/Toy.jsx
+++ src/Pages/Home/Toy/Toy.jsx
@@ -1,3 +1,5 @@
+import { Rating, ThinStar } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 import { Link } from "react-router-dom";
 
 const Toy = ({ toy }) => {
@@ -9,9 +11,23 @@ const Toy = ({ toy }) => {
         <img src={img} alt="" className="rounded-xl h-full" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">{name}</h2>
-        <p className="text-left">Price: ${price}</p>
-        <p className="text-left">Rating: {rating}</p>
+        <h2 className="card-title text-black">{name}</h2>
+        <p className="text-left text-lg font-semibold text-red-600">
+          Price: <span className="font-bold">${price}</span>
+        </p>
+        <div className="flex-grow flex text-lg">
+          <Rating
+            style={{ maxWidth: 100 }}
+            value={Math.round(rating || 0)}
+            itemStyles={{
+              itemShapes: ThinStar,
+              activeFillColor: "#ffb700",
+              inactiveFillColor: "#fbf1a9",
+            }}
+            readOnly
+          />
+          <span className="pl-2 font-semibold">{rating}</span>
+        </div>
         <div className="card-actions justify-end">
           <Link to={`toyDetails/${_id}`}>
             <button className="btn btn-primary">View Details</button>
