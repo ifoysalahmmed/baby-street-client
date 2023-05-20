35b1864db9062ad03caa49265804334e72eb0e84
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-05-20 15:32:34 +0600

    added SocialLogin Component

--- src/Pages/Shared/SocialLogin/SocialLogin.jsx
+++ src/Pages/Shared/SocialLogin/SocialLogin.jsx
@@ -1,7 +1,18 @@
+import useFirebase from "../../../Hooks/useFirebase";
+import googleImg from "../../../assets/images/icons/google.svg";
+
 const SocialLogin = () => {
+  const handleGoogleLogin = useFirebase();
+
   return (
-    <div>
-      <h2>Social login coming</h2>
+    <div className="text-center mt-4 space-y-3">
+      <div className="divider">Or Sign in with</div>
+      <button
+        onClick={handleGoogleLogin}
+        className="btn btn-circle btn-outline p-3"
+      >
+        <img src={googleImg} alt="" />
+      </button>
     </div>
   );
 };
