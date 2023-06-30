a2f849bcc6d4b5b014f2bb79edd99464d1ff144f
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-06-30 23:54:35 +0600

    updated Main Component

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -4,9 +4,9 @@ import Footer from "../Pages/Shared/Footer/Footer";
 
 const Main = () => {
   return (
-    <div>
+    <div className="pt-4">
       <Navbar></Navbar>
-      <div className="min-h-[calc(100vh-338px)]">
+      <div className="min-h-[calc(100vh-354px)]">
         <Outlet></Outlet>
       </div>
       <Footer></Footer>
